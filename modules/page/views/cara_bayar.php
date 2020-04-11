ee64863cc3b0d05d421e885c360e7d1fc6508d12
listawulandari <62386316+listawulandari@users.noreply.github.com>
2020-04-11 17:08:13 +0700

    Add files via upload

---
+++ modules/page/views/cara_bayar.php
@@ -0,0 +1,9 @@
+
+<h2>Cara Bayar</h2>
+	<div class="isi"  style="padding: 20px;">
+	<h4><b>Transfer melalui rekening Bank</b></h4>
+		<p>- ATM</p>
+    		<p>- Internet Banking</p>
+    			<p>- SMS Banking</p>
+    				<p>- Mobile Banking</p>
+    					</div>
